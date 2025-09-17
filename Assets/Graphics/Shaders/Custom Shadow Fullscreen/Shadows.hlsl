0f0088684d3b236beda99e17e1222735cc268c69
Andres Urrego <andres.urrego@polygon.us>
2025-09-17 17:39:37 -0500

    - haste shader edit - new noise gradient shader (noise and dither), need more testing

--- Assets/Graphics/Shaders/Custom Shadow Fullscreen/Shadows.hlsl
+++ Assets/Graphics/Shaders/Custom Shadow Fullscreen/Shadows.hlsl
@@ -4,7 +4,7 @@
 void SampleScreenSpaceShadow_half(half2 uv, out half shadow)
 {
     #if defined(SHADERGRAPH_PREVIEW)
-            shadow = 1.0;
+        shadow = 1.0;
     #else
         shadow = SAMPLE_TEXTURE2D(_ScreenSpaceShadowmapTexture, sampler_ScreenSpaceShadowmapTexture, uv).r;
     #endif
